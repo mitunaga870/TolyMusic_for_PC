8ef684fc6f27f105d2617b2f28a0aa5456976a87
umisoon <af21022@shibaura-it.ac.jp>
2023-05-27 00:01:18 +0900

    パケット取得実装中

---
+++ StreamingTest/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using CefSharp;
+using CefSharp.Wpf;
+using NAudio.Wave;
+
+namespace StreamingTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            //CefSharp設定
+            CefSharp.Wpf.CefSettings settings = new CefSharp.Wpf.CefSettings();
+            settings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 /CefSharp Browser" + Cef.CefSharpVersion;
+            settings.Locale = "ja";
+            settings.AcceptLanguageList = "ja,en-US;q=0.9,en;q=0.8";
+            settings.CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache");
+            settings.PersistSessionCookies = true;
+            Cef.Initialize(settings);
+        }
+        public void Loaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            /*
+            //browser
+            var browser = new ChromiumWebBrowser();
+            browser.RequestHandler = new YoutubeReqHandler(); 
+            browser.Address = "https://www.youtube.com/watch?v=XjfpkGSy8Q4";
+            Main.Children.Add(browser);
+            */
+            AudioFileReader reader = new AudioFileReader("test.weba");
+            BufferedWaveProvider provider = new BufferedWaveProvider(reader.WaveFormat);
+        }
+    }
+}
