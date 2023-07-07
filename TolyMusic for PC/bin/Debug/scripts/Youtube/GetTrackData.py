de75162a387a3aee16877ee070dc9a24485015af
mitsunaga870 <makingacounts4159@gmail.com>
2023-07-08 06:59:49 +0900

    Youtubetrackdata取得中

---
+++ TolyMusic for PC/bin/Debug/scripts/Youtube/GetTrackData.py
@@ -0,0 +1,10 @@
+﻿from ytmusicapi import YTMusic
+import io, sys
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+# API呼び出し
+yt = YTMusic("scripts\\Youtube\\yt_outh.json", language="ja")
+
+# ライブラリーの曲を取得
+SONG = yt.get_song("kzXj1ZMg9Gg")
